f559823e9c2f6e462b7373a72d282033e54af29f
Le Hai Son <lehaisonmath6@gmail.com>
2019-07-28 11:19:32 +0700

    update new

--- test/main.go
+++ test/main.go
@@ -1,25 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	backendmanager "github.com/OpenStars/GoEndpointBackendManager"
 )
 
 func main() {
-	enpManager := backendmanager.NewEndPointManager("http://127.0.0.1:2379", "/server-monitor/openstars/services/api/zshare/zsmetadataservice")
+	enpManager := backendmanager.NewEndPointManager("http://127.0.0.1:2379", "/openstars/services/api/zshare/zsmetadataservice")
 	enpManager.LoadEndpoint()
-	err, lsEnpoints := enpManager.GetEndPoints()
+	err, ep := enpManager.GetEndPointType(backendmanager.EThriftCompact)
 	if err != nil {
-		log.Println(err.Error())
+		log.Fatalln(err.Error())
 	}
-	CompactEndpoint := new(backendmanager.EndPoint)
-	for i := 0; i < len(lsEnpoints); i++ {
-		log.Println(lsEnpoints[i])
-		if lsEnpoints[i].Type == backendmanager.EThriftCompact {
-			CompactEndpoint = lsEnpoints[i]
-		}
-	}
-	fmt.Println("Compact host : ", CompactEndpoint.Host, " , port : ", CompactEndpoint.Port)
+	log.Println("host:", ep.Host, "; port:", ep.Port)
+	// err, lsEnpoints := enpManager.GetEndPoints(typ)
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+	// CompactEndpoint := new(backendmanager.EndPoint)
+	// for i := 0; i < len(lsEnpoints); i++ {
+	// 	log.Println(lsEnpoints[i])
+	// 	if lsEnpoints[i].Type == backendmanager.EThriftCompact {
+	// 		CompactEndpoint = lsEnpoints[i]
+	// 	}
+	// }
+	// fmt.Println("Compact host : ", CompactEndpoint.Host, " , port : ", CompactEndpoint.Port)
 }
