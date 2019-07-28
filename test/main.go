30177765fcba23953bd7396b52cd99884311df5c
Le Hai Son <lehaisonmath6@gmail.com>
2019-07-28 11:07:14 +0700

    update new

--- test/main.go
+++ test/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	backendmanager "github.com/OpenStars/GoEndpointBackendManager"
 )
 
 func main() {
 	enpManager := backendmanager.NewEndPointManager("http://127.0.0.1:2379", "/openstars/services/api/zshare/zsmetadataservice")
 	enpManager.LoadEndpoint()
+	err, lsEnpoints := enpManager.GetEndPoints()
+	if err != nil {
+		log.Println(err.Error())
+	}
+	CompactEndpoint := new(backendmanager.EndPoint)
+	for i := 0; i < len(lsEnpoints); i++ {
+		if lsEnpoints[i].Type == backendmanager.EThriftCompact {
+			CompactEndpoint = lsEnpoints[i]
+		}
+	}
+	fmt.Println("Compact host : ", CompactEndpoint.Host, " , port : ", CompactEndpoint.Port)
+	log.Println(lsEnpoints)
 }
